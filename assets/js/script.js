e3fe33d1ce48eaf591163953449820aa83a0a42f
KevinHenleyCode <kevinhenleyinfo@gmail.com>
2021-04-04 06:25:20 -0500

    sent the fetch data to new function to handle html elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,31 @@
+
+
 var userCity = $('button').on('click', function(city){
-    var city = $('textarea').val()
-    test(city)
+    var city = $('#city').val()
+    weatherData(city)
 })
 
 
-function test(city) {
-    
+function weatherData(city) {
     var siteURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=ba343b52255d8d308c7b0d444577c71e`
     var weatherInfo
-    
+
     fetch(siteURL)
     .then(response => response.json())
     .then(data => weatherInfo = data)
-    .then(() => 
-    
-    console.log(weatherInfo.main.temp));
+    .then(() => printTemp(weatherInfo, city))
+    .catch((error) => {
+        alert('Invalid city');
+        resetTextarea()
+    }) 
+}
+
+
+function printTemp(weatherInfo, city) {
+    $('.one').text("It's " + JSON.stringify(weatherInfo.main.temp) + " degrees in " + city + ".")
+}
+
+
+function resetTextarea() {
+    $('textarea').val('')
 }
