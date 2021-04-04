bdd45525b0382eac651dc76c1e4683093949dec9
KevinHenleyCode <kevinhenleyinfo@gmail.com>
2021-04-04 04:45:02 -0500

    added an on click event that grabs the input from the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-console.log('test');
+var userCity = $('button').on('click', function(city){
+    var city = $('textarea').val()
+    test(city)
+})
+
+
+function test(city) {
+    
+    var siteURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=ba343b52255d8d308c7b0d444577c71e`
+    var weatherInfo
+    
+    fetch(siteURL)
+    .then(response => response.json())
+    .then(data => weatherInfo = data)
+    .then(() => 
+    
+    console.log(weatherInfo.main.temp));
+}
