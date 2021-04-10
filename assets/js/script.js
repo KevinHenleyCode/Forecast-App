1d48bd261440bde1770dc02bb20f1857815c5b25
KevinHenleyCode <kevinhenleyinfo@gmail.com>
2021-04-10 02:00:55 -0500

    injected api data into the bootstrap card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var theDate = moment().format("M/DD/YYYY");
 
 
 var userCity = $('button').on('click', function(city){
@@ -9,7 +10,7 @@ var userCity = $('button').on('click', function(city){
 function weatherData(city) {
     var siteURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=ba343b52255d8d308c7b0d444577c71e`
     var weatherInfo
-
+    
     fetch(siteURL)
     .then(response => response.json())
     .then(data => weatherInfo = data)
@@ -17,12 +18,17 @@ function weatherData(city) {
     .catch((error) => {
         alert('Invalid city');
         resetTextarea()
-    }) 
+    })
 }
 
 
 function printTemp(weatherInfo, city) {
-    $('.one').text("It's " + JSON.stringify(weatherInfo.main.temp) + " degrees in " + city + ".")
+    console.log(weatherInfo);
+
+    $('.date').html(theDate)
+    $('.pic').html("<img src=http://openweathermap.org/img/wn/" + weatherInfo.weather[0].icon + ".png>")
+    $('.temp').html("Temp: " + JSON.stringify(weatherInfo.main.temp) + "°F")
+    $('.humid').html("Humidity: " + JSON.stringify(weatherInfo.main.humidity) + "%")
 }
 
 
