e60cc80ea21ee88aa51c8798c85a5c410867470c
KevinHenleyCode <kevinhenleyinfo@gmail.com>
2021-04-10 05:19:18 -0500

    added a second api call to handle future weather dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,21 @@
-var theDate = moment().format("M/DD/YYYY");
+let theDate = moment().format("M/DD/YYYY");
 
 
-var userCity = $('button').on('click', function(city){
-    var city = $('#city').val()
-    weatherData(city)
+let userSubmit = $('button').on('click', function(){
+    let userCity = $('#city').val()
+    cityApi(userCity)
 })
 
 
-function weatherData(city) {
-    var siteURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=ba343b52255d8d308c7b0d444577c71e`
-    var weatherInfo
+// gathers information on the city to obtain the latitude and longitude
+function cityApi(userCity) {
+    let siteURL = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&units=imperial&appid=ba343b52255d8d308c7b0d444577c71e`
+    let cityData
     
     fetch(siteURL)
     .then(response => response.json())
-    .then(data => weatherInfo = data)
-    .then(() => printTemp(weatherInfo, city))
+    .then(data => cityData = data)
+    .then(() => passLatLon(cityData, userCity))
     .catch((error) => {
         alert('Invalid city');
         resetTextarea()
@@ -22,13 +23,47 @@ function weatherData(city) {
 }
 
 
-function printTemp(weatherInfo, city) {
-    console.log(weatherInfo);
+function passLatLon(cityData, userCity) {
+    
+    let lat = cityData.coord.lat
+    let lon = cityData.coord.lon
+    weatherApi(userCity, lat, lon)
+} 
 
-    $('.date').html(theDate)
-    $('.pic').html("<img src=http://openweathermap.org/img/wn/" + weatherInfo.weather[0].icon + ".png>")
-    $('.temp').html("Temp: " + JSON.stringify(weatherInfo.main.temp) + "°F")
-    $('.humid').html("Humidity: " + JSON.stringify(weatherInfo.main.humidity) + "%")
+
+function weatherApi(userCity, lat, lon) {
+    let siteURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=ba343b52255d8d308c7b0d444577c71e`
+    let weatherData
+    
+    fetch(siteURL)
+    .then(response => response.json())
+    .then(data => weatherData = data)
+    .then(() => printUV(weatherData, userCity))
+    .catch((error) => {
+        alert('Invalid city');
+        resetTextarea()
+    })
+}
+
+
+function printUV(weatherData, userCity) {
+    console.log(weatherData);
+    let timestamp = weatherData.daily[1].dt
+    let futureDays = new Date(timestamp*1000)
+
+    $('.date').html(userCity + " (" + theDate + ") " + "<img src=http://openweathermap.org/img/wn/" + weatherData.current.weather[0].icon + ".png>")
+    $('.temp').html("Temperature: " + JSON.stringify(weatherData.current.temp) + " °F")
+    $('.humid').html("Humidity: " + JSON.stringify(weatherData.current.humidity) + "%")
+    $('.wind').html("Wind Speed: " + JSON.stringify(weatherData.current.wind_speed) + " MPH")
+    $('.uv').html("UVI : " + JSON.stringify(weatherData.daily[0].uvi))
+
+    console.log(`${futureDays.getUTCMonth()+1}/${futureDays.getUTCDate()}/${futureDays.getUTCFullYear()}`)
+
+        // for small cards
+// $('.date').html(theDate)
+// $('.pic').html("<img src=http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + ".png>")
+// $('.temp').html("Temp: " + JSON.stringify(weatherData.main.temp) + "°F")
+// $('.humid').html("Humidity: " + JSON.stringify(weatherData.main.humidity) + "%")
 }
 
 
